ae1f2878120fef98806718faa5b7badfe4ab7f13
root <abramainnaresh4@gmail.com>
2023-09-12 05:26:48 +0000

    NodeJS Todays Date

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  let date = new Date();
+  response.send(
+    `Today date as a ${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
+  );
+});
+
+app.listen(3000);
